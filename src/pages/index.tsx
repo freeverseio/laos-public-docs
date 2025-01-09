d847c1429e29c9aced872529db49b5352381fd77
Toni <>
2025-01-09 15:24:34 +0100

    further simplifications of landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,8 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 import { Redirect } from '@docusaurus/router';
 
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return <Redirect to="/landing" />;
-
-}
-
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  const { siteConfig } = useDocusaurusContext();
+
+  return <Redirect to="/landing" />;
 }
