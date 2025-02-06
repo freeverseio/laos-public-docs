bff117eb8dd0610ab001e7058a3be78d76298f35
Toni <>
2025-02-06 12:39:21 +0100

    porting first part of tokenomics

---
+++ sidebarsTokenomics.ts
@@ -0,0 +1,10 @@
+// sidebarsApi.ts
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebarsTokenomics: SidebarsConfig = {
+  tokenomicsSidebar: [
+    'laos-utility-token',
+  ],
+};
+
+export default sidebarsTokenomics;
